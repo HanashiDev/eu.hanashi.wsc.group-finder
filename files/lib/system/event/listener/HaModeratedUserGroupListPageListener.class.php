9250e5832ffa60d7dcdc5763ecc77223f25dd374
Peter Lohse <peterlohse@yahoo.de>
2019-01-18 19:06:25 +0100

    Bugfix

--- files/lib/system/event/listener/HaModeratedUserGroupListPageListener.class.php
+++ files/lib/system/event/listener/HaModeratedUserGroupListPageListener.class.php
@@ -1,25 +1,36 @@
 <?php
 namespace wcf\system\event\listener;
+use wcf\system\request\LinkHandler;
 use wcf\system\WCF;
+use wcf\util\HeaderUtil;
 
 class HaModeratedUserGroupListPageListener implements IParameterizedEventListener {
     public function execute($eventObj, $className, $eventName, array &$parameters) {
-        if (isset($_POST['haGroupName'])) {
-            $eventObj->objectList->getConditionBuilder()->add('user_group.groupName LIKE ?', ['%'.$_POST['haGroupName'].'%']);
+        if ($eventName == 'afterInitObjectList') {
+            if (isset($_POST['haGroupName'])) {
+                $eventObj->objectList->getConditionBuilder()->add('user_group.groupName LIKE ?', ['%'.$_POST['haGroupName'].'%']);
 
-            WCF::getSession()->register('haGroupName', $_POST['haGroupName']);
+                WCF::getSession()->register('haGroupName', $_POST['haGroupName']);
 
-            WCF::getTpl()->assign([
-                'haGroupName' => $_POST['haGroupName']
-            ]);
-        } else if (!empty(WCF::getSession()->getVar('haGroupName'))) {
-            $haGroupName = WCF::getSession()->getVar('haGroupName');
+                WCF::getTpl()->assign([
+                    'haGroupName' => $_POST['haGroupName']
+                ]);
+            } else if (!empty(WCF::getSession()->getVar('haGroupName'))) {
+                $haGroupName = WCF::getSession()->getVar('haGroupName');
 
-            $eventObj->objectList->getConditionBuilder()->add('user_group.groupName LIKE ?', ['%'.$haGroupName.'%']);
+                $eventObj->objectList->getConditionBuilder()->add('user_group.groupName LIKE ?', ['%'.$haGroupName.'%']);
 
-            WCF::getTpl()->assign([
-                'haGroupName' => $haGroupName
-            ]);
+                WCF::getTpl()->assign([
+                    'haGroupName' => $haGroupName
+                ]);
+            }
+        } else if ($eventName == 'countItems') {
+            if ($eventObj->objectList->countObjects() == 0 && !empty(WCF::getSession()->getVar('haGroupName'))) {
+                WCF::getSession()->unregister('haGroupName');
+
+                HeaderUtil::delayedRedirect(LinkHandler::getInstance()->getLink('ModeratedUserGroupList'), WCF::getLanguage()->getDynamicVariable('wcf.moderated.hanashi.noResult'));
+		        exit;
+            }
         }
     }
 }
--- files/lib/system/event/listener/HaUserGroupListPageListener.class.php
+++ files/lib/system/event/listener/HaUserGroupListPageListener.class.php
@@ -1,25 +1,36 @@
 <?php
 namespace wcf\system\event\listener;
+use wcf\system\request\LinkHandler;
 use wcf\system\WCF;
+use wcf\util\HeaderUtil;
 
 class HaUserGroupListPageListener implements IParameterizedEventListener {
     public function execute($eventObj, $className, $eventName, array &$parameters) {
-        if (isset($_POST['haUserGroupName'])) {
-            $eventObj->objectList->getConditionBuilder()->add('user_group.groupName LIKE ?', ['%'.$_POST['haUserGroupName'].'%']);
+        if ($eventName == 'afterInitObjectList') {
+            if (isset($_POST['haUserGroupName'])) {
+                $eventObj->objectList->getConditionBuilder()->add('user_group.groupName LIKE ?', ['%'.$_POST['haUserGroupName'].'%']);
 
-            WCF::getSession()->register('haUserGroupName', $_POST['haUserGroupName']);
+                WCF::getSession()->register('haUserGroupName', $_POST['haUserGroupName']);
 
-            WCF::getTpl()->assign([
-                'haUserGroupName' => $_POST['haUserGroupName']
-            ]);
-        } else if (!empty(WCF::getSession()->getVar('haUserGroupName'))) {
-            $haUserGroupName = WCF::getSession()->getVar('haUserGroupName');
+                WCF::getTpl()->assign([
+                    'haUserGroupName' => $_POST['haUserGroupName']
+                ]);
+            } else if (!empty(WCF::getSession()->getVar('haUserGroupName'))) {
+                $haUserGroupName = WCF::getSession()->getVar('haUserGroupName');
 
-            $eventObj->objectList->getConditionBuilder()->add('user_group.groupName LIKE ?', ['%'.$haUserGroupName.'%']);
+                $eventObj->objectList->getConditionBuilder()->add('user_group.groupName LIKE ?', ['%'.$haUserGroupName.'%']);
 
-            WCF::getTpl()->assign([
-                'haUserGroupName' => $haUserGroupName
-            ]);
+                WCF::getTpl()->assign([
+                    'haUserGroupName' => $haUserGroupName
+                ]);
+            }
+        } else if ($eventName == 'countItems') {
+            if ($eventObj->objectList->countObjects() == 0 && !empty(WCF::getSession()->getVar('haUserGroupName'))) {
+                WCF::getSession()->unregister('haUserGroupName');
+
+                HeaderUtil::delayedRedirect(LinkHandler::getInstance()->getLink('UserGroupList'), WCF::getLanguage()->getDynamicVariable('wcf.moderated.hanashi.noResult'));
+                exit;
+            }
         }
     }
 }
